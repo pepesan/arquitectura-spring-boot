bebd0efb056c57c6e72c9360352a7b76613fb45c
pepesan <pepesan@gmailcom>
2019-02-21 16:35:11 +0100

    mejorado ejemplo de seguridad

---
+++ SpringBootJwtAuthentication/src/main/java/com/cursosdedesarrollo/apitoken/model/Employee.java
@@ -0,0 +1,40 @@
+package com.cursosdedesarrollo.apitoken.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * @author JavaSolutionsGuide
+ *
+ */
+@Data
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="EMPLOYEE_NAME")
+    private String name;
+
+    @Column(name="EMPLOYEE_SALARY")
+    private Integer salary;
+
+    @Column(name="DEPARTMENT")
+    private String department;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
+
